0ad446d7cc7cf371ea450de52f3a403f39ac6a9c
Ramill <ramill_ibragimov@icloud.com>
2022-05-26 10:53:52 +0300

    Initial Commit

---
+++ OrientationDetectionInSwiftUI/OrientationDetectionInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationDetectionInSwiftUIApp.swift
+//  OrientationDetectionInSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct OrientationDetectionInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
