edfc23046c9fd923091f2036dd178d96268290c8
Ramill <ramill_ibragimov@icloud.com>
2022-05-26 11:06:02 +0300

    init

--- OrientationDetectionInSwiftUI/ContentView.swift
+++ OrientationDetectionInSwiftUI/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var orientation = UIDevice.current.orientation
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if orientation.isLandscape {
+                HStack {
+                    Image(systemName: "heart.fill")
+                    Text("Detect Orientation")
+                    Image(systemName: "heart.fill")
+                }
+                .font(.largeTitle)
+                .foregroundColor(.purple)
+            } else {
+                VStack {
+                    Image(systemName: "heart.fill")
+                    Text("Detect Orientation")
+                    Image(systemName: "heart.fill")
+                }
+                .font(.largeTitle)
+                .foregroundColor(.purple)
+            }
+        }
+        .detectOrientation($orientation)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DetectOrientation: ViewModifier {
+    
+    @Binding var orientation: UIDeviceOrientation
+    
+    func body(content: Content) -> some View {
+        content
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                orientation = UIDevice.current.orientation
+            }
+    }
+}
+
+extension View {
+    func detectOrientation(_ orientation: Binding<UIDeviceOrientation>) -> some View {
+        modifier(DetectOrientation(orientation: orientation))
     }
 }
